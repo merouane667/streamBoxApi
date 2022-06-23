3b44674ef845e5d1964169e47a8a94ff0a8191bc
merouane hany <merouane.hany@ynov.com>
2022-06-23 21:34:26 +0100

    1.0 version

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function _construct(){
+        $this->middleware('auth:api',['except'=>['login','register']]);
+    }
+    public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|string|email|unique:users',
+            'image'=>'string',
+            'password'=>'required|string|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(),400);
+        }
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->image = $request->image;
+        $user->save();
+
+        return response()->json([
+            'message'=>'User successfully registered',
+            'user'=>$user
+        ],201);
+    }
+    public function login(Request $request){
+        $validator = Validator::make($request->all(),[ 
+            'email'=>'required|email',
+            'password'=>'required|string|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+        if (!$token=auth()->attempt($validator->validated())) {
+            return response()->json(['error'=>'Unauthorized'],401);
+        }
+        return $this->createNewToken($token);
+    }
+    public function createNewToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>auth()->factory()->getTTL()*60,
+            'user'=>auth()->user()
+        ]);
+    }
+    public function profile(){
+        return response()->json(auth()->user());
+    }
+    public function logout(){
+        auth()->logout();
+        return response()->json([
+            'message'=>'User logged out',
+        ]);
+    }
+}
